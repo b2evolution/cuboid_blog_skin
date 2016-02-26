e2d1f7c062487db743b869c0f27cdb9c426ff3dd
hhren1984 <>
2016-02-26 15:30:27 +0800

    Change the background image to an easy choose style

--- _skin.class.php
+++ _skin.class.php
@@ -81,6 +81,56 @@ class cuboid_blog_Skin extends Skin
      );
      return $supported_kinds;
    }
+        /**
+         * Judge if the file is the image we want to use
+         *
+	 * @param string filepath: the path of a file
+         *         array arr_types: the file type we want to use
+	 * @return array
+	 */
+        function isImage( $filepath, $arr_types=array( ".gif", ".jpeg", ".png", ".bmp" ) )
+        {
+                if(file_exists($filepath))
+                {
+                        $info = getimagesize($filepath);
+                        $ext = image_type_to_extension($info['2']);
+                        return in_array($ext,$arr_types);
+                }else
+                {
+                        return false;
+                }
+        }
+
+        /**
+         * Get the pictures of one local folder as an array
+         *
+	 * @param string img_folder; the image folder;
+         *         string img_folder_url; folder url, we would like to show the img of this folder on the screen for user viewing;
+	 *         int thumb_width: thumb image whdth shown on the skin setting page
+	 *         int thumb_height: thumb image height shown on the skin setting page
+	 * @return array
+	 */
+        function get_arr_pics_from_folder( $img_folder, $img_folder_url, $thumb_width = 50, $thumb_height = 50 )
+        {
+                $arr_filenames = $filesnames =array();
+                if(file_exists($img_folder))
+                {
+                      $filesnames = scandir($img_folder);  
+                }
+                $count = 0;
+                foreach ( $filesnames as $name ) 
+                {
+                        $count++;
+                        if ( $name != "." && $name != ".." && $name != "_evocache" && $this->isImage($img_folder.$name) ) //not the folder and other files
+                        {
+                              $arr_filenames[] = array( $img_folder_url.$name, 
+                                  "<a href='".$img_folder_url.$name."' target='blank'><img src='".$img_folder_url.$name."' width=".$thumb_width."px heigh=".$thumb_height."px /></a>" );                               
+                        }
+                        if ($count==30) break; // The max number of the images we want to show
+                }
+                $arr_filenames[] = array("none",T_("Transparent"));
+                return $arr_filenames;
+        }
 
 
 	/**
@@ -91,9 +141,15 @@ class cuboid_blog_Skin extends Skin
 	 */
 	function get_param_definitions( $params )
 	{
-
+            global $Blog;
       // Load to use function get_available_thumb_sizes()
       load_funcs( 'files/model/_image.funcs.php' );
+      // System provide bg images
+      $bodybg_cat = 'assets/images/bodybg/'; // Background images folder relative to this skin folder
+      $arr_bodybg = $this -> get_arr_pics_from_folder( $this->get_path().$bodybg_cat, $this->get_url().$bodybg_cat, 50, 50 );
+      // User Custom bg images
+      $custom_bodybg_cat  = "bodybg/"; // Background images folder which created by users themselves, and it's relative to collection media dir
+      $arr_custom_bodybg = $this->get_arr_pics_from_folder( $Blog->get_media_dir().$custom_bodybg_cat, $Blog->get_media_url().$custom_bodybg_cat, 50 ,50);
 
 		$r = array_merge( array(
             'general_settings_start' => array(
@@ -135,22 +191,23 @@ class cuboid_blog_Skin extends Skin
                   'options'  => array(
                      array( 'color', T_('Background Color') ),
                      array( 'images', T_('Image Pattern') ),
+                     array( 'custom_images', T_('User Custom Image') ),
                   ),
                   'defaultvalue' => 'images',
                ),
                'bg_image' => array(
                   'label'    => T_('Background Image Pattern'),
                   'note'     => T_('Choose your favorite background image pattern'),
-                  'type'     => 'select',
-                  'options'  => array(
-                     'bg_1'     => T_('Pattern 1'),
-                     'bg_2'     => T_('Pattern 2'),
-                     'bg_3'     => T_('Pattern 3'),
-                     'bg_4'     => T_('Pattern 4'),
-                     'bg_5'     => T_('Pattern 5'),
-                     'bg_6'     => T_('Pattern 6'),
-                  ),
-                  'defaultvalue' => 'bg_1',
+                  'type'     => 'radio',
+                  'options'  => $arr_bodybg,
+                  'defaultvalue' => reset($arr_bodybg[0]),
+               ),
+               'bg_image_custom' => array(
+                   'label'    => T_('User Custom Background Image'),                               
+                   'note'     => T_('（Please create a folder named <b><i>'.str_replace("/","",$custom_bodybg_cat).'</i></b> in your collection media folder and put the images into it. Now <a href="admin.php?ctrl=files" target="_blank"><i>Create folder or Upload images</i></a>）'),                           
+                   'type'     => 'radio',                        
+                   'options'  => $arr_custom_bodybg,                   
+                   'defaultvalue' => reset($arr_custom_bodybg[0]),
                ),
                'site_background_color' => array(
                   'label'        => T_('Site Background Color'),
@@ -870,31 +927,24 @@ class cuboid_blog_Skin extends Skin
       // Site Background
       $bg_image = $this->get_setting( 'bg_image' );
       if ( $this->get_setting( 'background_type' ) == 'images' && $bg_image ) {
-         switch( $bg_image ) {
-            case 'bg_1': // When regular layout is chosen, nothing happens, since regular is default
-               $custom_css .= 'body { background-image: url("assets/images/bg-1.png"); }';
-            break;
-
-            case 'bg_2':
-               $custom_css .= 'body { background-image: url("assets/images/bg-2.png"); }';
-            break;
-
-            case 'bg_3':
-               $custom_css .= 'body { background-image: url("assets/images/bg-3.png"); }';
-            break;
-
-            case 'bg_4':
-               $custom_css .= 'body { background-image: url("assets/images/bg-4.png"); }';
-            break;
-
-            case 'bg_5':
-               $custom_css .= 'body { background-image: url("assets/images/bg-5.png"); }';
-            break;
-
-            case 'bg_6':
-               $custom_css .= 'body { background-image: url("assets/images/bg-6.png"); }';
-            break;
-         }
+            if($bg_image == "none") 
+            {
+                    $custom_css .= "body { background: transparent; }";
+            }else 
+            {
+                    $custom_css .= "body { background-image: url('".$bg_image."');}";
+            }     
+      }
+      // User custom bg images setting
+      $bg_image_custom = $this->get_setting( 'bg_image_custom' );
+      if ( $this->get_setting( 'background_type' ) == 'custom_images' && $bg_image_custom ) {
+            if($bg_image_custom == "none") 
+            {
+                    $custom_css .= "body { background: transparent; }";
+            }else 
+            {
+                    $custom_css .= "body { background-image: url('".$bg_image_custom."');}";
+            }  
       }
 
       if ( $this->get_setting( 'background_type' ) == 'color' ) {
