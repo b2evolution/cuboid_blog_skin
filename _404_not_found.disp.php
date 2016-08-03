d236aa3201716bdf269f8305923662088770019a
Ariflaw <nurariflaw@gmail.com>
2016-08-03 20:45:40 +0700

    Update

--- _404_not_found.disp.php
+++ _404_not_found.disp.php
@@ -19,6 +19,28 @@ global $disp_detail, $baseurl, $app_name;
 
 <div class="error_404">
    <i class="fa fa-warning" aria-hidden="true"></i>
-   <h2>404 Not Found</h2>
-   <p><a href="<?php echo $baseurl; ?>"><?php echo $app_name; ?></a> cannot resolve the requested URL.</p>
+   <?php
+       skin_container( /* TRANS: Widget container name */ NT_('404 Page'), array(
+           'widget_context' 			 => 'item',	// Signal that we are displaying within an Item
+           // The following (optional) params will be used as defaults for widgets included in this container:
+           // This will enclose each widget in a block:
+           'block_start' 				 => '<div class="$wi_class$">',
+           'block_end' 				     => '</div>',
+           // This will enclose the title of each widget:
+           'block_title_start' 		     => '<h3>',
+           'block_title_end' 			 => '</h3>',
+           // Template params for "Item Tags" widget
+           'widget_item_tags_before'     => '<div class="small">'.T_('Tags').': ',
+           'widget_item_tags_after'      => '</div>',
+           // Params for skin file "_item_content.inc.php"
+           'widget_item_content_params'  => $params,
+           // Search
+           'search_input_before'   => '<div class="input-group">',
+           'search_input_after'    => '',
+           'search_submit_before'  => '<span class="input-group-btn">',
+           'search_submit_after'   => '</span></div>',
+       ) );
+   ?>
+   <!-- <h2>404 Not Found</h2>
+   <p><a href="<?php echo $baseurl; ?>"><?php echo $app_name; ?></a> cannot resolve the requested URL.</p> -->
 </div>
