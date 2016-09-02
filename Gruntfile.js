020596caa9998ec1fa175057280bdeaa8ca4929c
Ariflaw <nurariflaw@gmail.com>
2016-09-02 09:54:16 +0700

    Update

--- Gruntfile.js
+++ Gruntfile.js
@@ -100,7 +100,7 @@ module.exports = function(grunt) {
            options: {
              watchTask: true,
             //  Change your url Deploy
-             proxy: "http://localhost/b2evo/stain/index.php/",
+             proxy: "http://localhost/b2evo/dev",
            }
          }
       },
