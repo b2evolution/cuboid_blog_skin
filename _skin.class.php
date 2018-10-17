603914f3f436719d2727e0d2584b6f021b52bbac
Erwin Rommel H. Satingin <winskie@gmail.com>
2018-10-17 10:18:47 +0800

    Updated declaration of get_default_widgets function.

--- _skin.class.php
+++ _skin.class.php
@@ -113,9 +113,12 @@ class cuboid_blog_Skin extends Skin
 
 
 	/**
-	 * Get the declarations of the widgets that the skin wants to use.
+	 * Get the declarations of the widgets that the skin recommends by default.
 	 *
-	 * @param string Collection kind: 'std', 'main', 'photo', 'group', 'forum', 'manual'
+	 * The skin class defines a default set of widgets to used. Skins should override this.
+	 *
+	 * @param string Collection type: 'std', 'main', 'photo', 'group', 'forum', 'manual'
+	 * @param string Skin type: 'normal' - Standard, 'mobile' - Phone, 'tablet' - Tablet
 	 * @param array Additional params. Example value 'init_as_blog_b' => true
 	 * @return array Array of default widgets:
 	 *          - Key - Container code,
@@ -130,10 +133,11 @@ class cuboid_blog_Skin extends Skin
 	 *                - 'params' - Widget params(array or serialized string),
 	 *                - 'type' - Widget type(default = 'core', another value - 'plugin'),
 	 *                - 'enabled' - Boolean value; default is TRUE; FALSE to install the widget as disabled,
-	 *                - 'coll_type': Include this widget only for collection kinds separated by comma, first char "-" means to exclude,
+	 *                - 'coll_type': Include this widget only for collection types separated by comma, first char "-" means to exclude,
+	 *                - 'skin_type': Include this widget only for skin types separated by comma, first char "-" means to exclude,
 	 *                - 'install' - Boolean value; default is TRUE; FALSE to skip this widget on install.
 	 */
-	function get_default_widgets( $kind = '', $context = array() )
+	function get_default_widgets( $coll_type, $skin_type = 'normal', $context = array() )
 	{
 		global $DB;
 
